b8a6e66f605a56967ba3949dc30189066f64d259
Janithya Priyabhashana <janithya.pr@gmail.com>
2026-05-25 21:39:06 +0530

    created a sample gin api.

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,1 +1,28 @@
 package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	gin.SetMode(gin.ReleaseMode)
+	r := gin.New()
+
+	// r.Use(
+	// 	middleware.RequestID(),
+	// 	middleware.Logging(log),
+	// 	middleware.Recovery(),
+	// )
+
+	r.GET("/health", func(c *gin.Context) {
+
+		// log.Info("health check")
+
+		c.JSON(http.StatusOK, gin.H{
+			"message": "healthy..",
+		})
+	})
+	r.Run(":8080")
+}
